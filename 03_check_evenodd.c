02bb1fac98ad292f98d409013468eda3fd9cff29
Sanskriti papneja <kritipapneja@gmail.com>
2026-08-27 11:03:22 +0530

    Create 03_check_evenodd.c

---
+++ 03_check_evenodd.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int num;
+    printf("\n enter number ");
+    scanf(" %d",&num);
+    if(num%2==0)
+       printf("\n number is even");
+    else
+       printf("\n number is odd");
+    return 0;
+}
