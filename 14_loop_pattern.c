6eafb3eebfde85ab9db36bf61a5956dbecf41d5f
Sanskriti papneja <kritipapneja@gmail.com>
2026-08-31 17:29:22 +0530

    Create 14_loop_pattern.c

---
+++ 14_loop_pattern.c
@@ -0,0 +1,14 @@
+// print pattern 
+#include <stdio.h>
+int main()
+{
+    int i , j ;
+    for(i=1;i<=5;i++)
+    {
+        printf("\n Pass- %d",i);
+        for(j=1;j<=5;j++)
+        {
+            printf(" %d",j);
+        }
+    }
+}
